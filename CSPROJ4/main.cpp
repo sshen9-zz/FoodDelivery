fb6766c3991f73cbf3299a5e3fc9be551f68da85
Samuel Shen <sam.shen321@gmail.com>
2020-03-04 13:49:22 -0800

    initial commit

---
+++ CSPROJ4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSPROJ4
+//
+//  Created by Sam Shen on 3/4/20.
+//  Copyright © 2020 CS Class. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
